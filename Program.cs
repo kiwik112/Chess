0572c3eb89fffd63a7adb1d51eb8897fcb3a4888
kiwik112 <pakiwi06@gmail.com>
2023-03-08 22:03:59 +0100

    Added incomplete basic move checking

--- Program.cs
+++ Program.cs
@@ -37,7 +37,8 @@ namespace Ches
                 Draw();
                 if (err) Console.WriteLine("u done messed up fam");
                 err = true;
-                if (turn) Console.WriteLine("\nBlack's turn > ");
+
+                if (turn) Console.Write("\nBlack's turn > ");
                 else Console.Write("\nWhite's turn > ");
                 string? input = Console.ReadLine();
                 if (input == null) continue;
@@ -51,17 +52,24 @@ namespace Ches
                         Environment.Exit(0);
                         break;
                 }
+
                 char[] parts = input.ToCharArray();
                 if (parts.Length != 4) continue;
                 if (parts[0] < 'a' || parts[0] > 'h' || parts[1] < '1' || parts[1] > '8' || parts[2] < 'a' || parts[2] > 'h' || parts[3] < '1' || parts[3] > '8') continue;
-                int x1 = (int)parts[0] - (int)'a';
-                int y1 = 7 - ((int)parts[1] - (int)'1');
-                int x2 = (int)parts[2] - (int)'a';
-                int y2 = 7 - ((int)parts[3] - (int)'1');
-                Debug.WriteLine($"{y1}, {x1} to {y2}, {x2}");
+
+                byte x1 = (byte)(parts[0] - 'a');
+                byte y1 = (byte)(7 - (parts[1] - '1'));
+                byte x2 = (byte)(parts[2] - 'a');
+                byte y2 = (byte)(7 - (parts[3] - '1'));
+
+                if (IsIllegal(board, turn, x1, y1, x2, y2)) continue;
+
+                if (board[y1, x1] == 0) continue;
                 board[y2, x2] = board[y1, x1];
                 board[y1, x1] = 0;
+
                 err = false;
+                turn = !turn;
             }
         }
         static void Draw()
@@ -70,12 +78,21 @@ namespace Ches
             Console.WriteLine("Chess by Pavel\n");
             Console.WriteLine("    a   b   c   d   e   f   g   h");
             Console.WriteLine("  ---------------------------------");
-            for (int i = 0; i < 8; i++)
+            for (byte i = 0; i < 8; i++)
             {
                 Console.WriteLine($"{8 - i} | {pieces[board[i, 0]]} | {pieces[board[i, 1]]} | {pieces[board[i, 2]]} | {pieces[board[i, 3]]} | {pieces[board[i, 4]]} | {pieces[board[i, 5]]} | {pieces[board[i, 6]]} | {pieces[board[i, 7]]} | {8 - i}");
                 Console.WriteLine("  ---------------------------------");
             }
             Console.WriteLine("    a   b   c   d   e   f   g   h");
+        }
+        static bool IsIllegal(byte[,] board, bool turn, byte x1, byte y1, byte x2, byte y2)
+        {
+            bool illegal = false;
+
+            if (board[y1, x1] > 6 ^ turn) illegal = true;
+            if ((board[y2, x2] < 7 && board[y2, x2] > 0) ^ turn) illegal = true;
+
+            return illegal;
         }
     }
 }
